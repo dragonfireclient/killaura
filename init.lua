8442dc0b5f4094f313da029b90ff136939619db1
Elias Fleckenstein <eliasfleckenstein@web.de>
2021-01-07 10:29:23 +0100

    Fixed crash

--- init.lua
+++ init.lua
@@ -3,7 +3,7 @@ minetest.register_globalstep(function(dtime)
 	if not player then return end
 	local control = player:get_control()
 	if minetest.settings:get_bool("killaura") or minetest.settings:get_bool("forcefield") and control.dig then
-		local friendlist = minetest.settings:get("friendlist"):split(",")
+		local friendlist = (minetest.settings:get("friendlist") or ""):split(",")
 		for _, obj in ipairs(minetest.get_objects_inside_radius(player:get_pos(), 5)) do
 			local do_attack = true
 			if obj:is_local_player() then
@@ -22,7 +22,7 @@ minetest.register_globalstep(function(dtime)
 		end
 	end
 end)
- 
+
 minetest.register_list_command("friend", "Configure Friend List (friends dont get attacked by Killaura or Forcefield)", "friendlist")
 
 minetest.register_cheat("Killaura", "Combat", "killaura")
